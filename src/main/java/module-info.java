ad9ccda9a3a64884db5f845777b0887267c6cab2
Ashkaaaria <thearianawshin@gmail.com>
2026-02-03 22:21:21 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.finallyy {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.finallyy to javafx.fxml;
+    exports org.example.finallyy;
+}
